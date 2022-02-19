3ddbc9184dbab8a6d102cc1b576c0db8455bf3ce
Maicon Renildo <maicon@lunes.io>
2022-02-18 21:42:36 -0300

    changed PackApp of 05-Packages and added image

--- 05-Packages/PackApp.java
+++ 05-Packages/PackApp.java
@@ -13,12 +13,8 @@ class PackApp {
 }
 
 class PackFrame extends JFrame {
-    Rect r1;
-    Rect r2;
-    Rect r3;
-    Ellipse e1;
-    Ellipse e2;
-    Ellipse e3;
+    Rect r1,r2,r3;
+    Ellipse e1,e2,e3;
     PackFrame () {
         this.addWindowListener (
             new WindowAdapter() {
@@ -29,16 +25,17 @@ class PackFrame extends JFrame {
         );
         this.setTitle("Java Packages");
         this.setSize(350, 350);
+        
         this.r1 = new Rect(50,50, 50,50,Color.blue,Color.black);
         this.r2 = new Rect(110,50, 50,30,Color.black,Color.pink);
-        this.r3 = new Rect(210,50, 50,30,new Color(153, 255, 204),Color.black);
+        this.r3 = new Rect(210,50, 50,30,new Color(153, 255, 204),Color.red);
 
         this.e1 = new Ellipse(50,300, 50,30,Color.red,Color.green);
-        this.e2 = new Ellipse(110,300, 50,30,Color.pink,Color.green);
-        this.e3 = new Ellipse(150,150, 100,30,Color.red,Color.green);
+        this.e2 = new Ellipse(110,300, 50,30,Color.blue,Color.orange);
+        this.e3 = new Ellipse(150,150, 100,30,Color.red,Color.blue);
     }
 
-    public void hi (Graphics g) {
+    public void paint (Graphics g) {
         super.paint(g);
         this.r1.paint(g);
         this.r2.paint(g);
